097d809d2f88a0ab095cb189df70d565d16dc910
MariaRipley <roblesb.maria@gmail.com>
2023-05-26 11:20:30 +0200

    Add objects and fix renderKitten function

--- js/main.js
+++ js/main.js
@@ -1,56 +1,61 @@
 "use strict";
 
-const catName1 = "anastacio";
-const catRace1 = "";
-const catDescription1 = `Porte elegante, su patrón de color tan característico y sus ojos
-                de un azul intenso, pero su historia se remonta a Asía al menos
-                hace 500 años, donde tuvo su origen muy posiblemente.`;
-const catImage1 = "https://dev.adalab.es/gato-siames.webp";
-
-const catName2 = "fiona";
-const catRace2 = "Sphynx";
-const catDescription2 = `Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
-              hasta con pinta de alienígena han llegado a definir a esta raza
-              gatuna que se caracteriza por la «ausencia» de pelo.`;
-const catImage2 = "https://dev.adalab.es/sphynx-gato.webp";
-
-const catName3 = "Cielo";
-const catRace3 = "Maine Coon";
-const catDescription3 = `Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
-              bella mirada se ha convertido en una de sus señas de identidad.
-              Sus ojos son grandes y las orejas resultan largas y en punta.`;
-const catImage3 = "https://dev.adalab.es/maine-coon-cat.webp";
 const elementList = document.querySelector(".list");
 elementList.classList.add("js-list");
 
+const kittenData = {
+  kittenData1: {
+    image: 'https://dev.adalab.es/gato-siames.webp',
+    name: 'anastacio',
+    desc: `Porte elegante, su patrón de color tan característico y sus  ojos de un azul intenso, pero su historia se remonta a Asía al menos
+    hace 500 años, donde tuvo su origen muy posiblemente.`,
+    race: '',
+  },
+  kittenData2: {
+    image: 'https://dev.adalab.es/sphynx-gato.webp',
+    name: 'fiona',
+    desc: `Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
+    hasta con pinta de alienígena han llegado a definir a esta raza
+    gatuna que se caracteriza por la «ausencia» de pelo.`,
+  race: 'Sphynx',
+  },
+  kittenData3: {
+    image: 'https://dev.adalab.es/maine-coon-cat.webp',
+    name: 'Cielo',
+    desc: `Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
+    bella mirada se ha convertido en una de sus señas de identidad.
+    Sus ojos son grandes y las orejas resultan largas y en punta.`,
+  race: 'Maine Coon',
+  }
+}
+
+
 //crear cards con gatitos
-const kittenOne = renderKitten(catImage1, catDescription1, catName1, catRace1);
-const kittenTwo = renderKitten(catImage2, catDescription2, catName2, catRace2);
-const kittenThree = renderKitten(
-  catImage3,
-  catDescription3,
-  catName3,
-  catRace3
-);
+const kittenOne = renderKitten(kittenData.kittenData1.image, kittenData.kittenData1.name, kittenData.kittenData1.race, kittenData.kittenData1.desc,);
+const kittenTwo = renderKitten(kittenData.kittenData2.image, kittenData.kittenData2.name, kittenData.kittenData2.race, kittenData.kittenData2.desc,);
+const kittenThree = renderKitten(kittenData.kittenData3.image, kittenData.kittenData3.name, kittenData.kittenData3.race, kittenData.kittenData3.desc,);
 
 elementList.innerHTML = kittenOne + kittenTwo + kittenThree;
 
-function renderKitten(url, description, name, race) {
+function renderKitten(image, name, race, desc) {
   return `<li class="card">
             <article>
               <img
                 class="card_img"
-                src= ${url}
+                src= ${image}
                 alt="siames-cat"
               />
               <h3 class="card_title">${name.toUpperCase()}</h3>
               <h4 class="card_race">${race}</h4>
               <p class="card_description">
-                ${description}
+                ${desc}
               </p>
             </article>
           </li>`;
 }
+
+
+
 
 //Buscar/filtrar
 const input_search_desc = document.querySelector(".js_in_search_desc");
@@ -63,22 +68,34 @@ const filterKitten = (event) => {
   const descrSearchText = input_search_desc.value;
   const raceSearchText = input_search_raza.value;
   if (descrSearchText != "" || raceSearchText != "") {
-    if (catDescription1.includes(descrSearchText)) {
-      if(cardRace.value != ""){
-        cardRace.value = "Uy que despiste, no sabemos su raza";
+    if (kittenData.kittenData1.desc.includes(descrSearchText)) {
+      if(kittenData.kittenData1.race != ""){
         elementList.innerHTML += kittenOne;
       } else{
         elementList.innerHTML += kittenOne;
+        const cardRace = document.querySelector('.card_race');
+        cardRace.innerHTML = 'Uy que despiste, no sabemos su raza';
       }
-      
     }
-    if (catDescription2.includes(descrSearchText)) {
+    if (kittenData.kittenData2.desc.includes(descrSearchText)) {
+      if(kittenData.kittenData2.race != ""){
       elementList.innerHTML += kittenTwo;
+    } else{
+        elementList.innerHTML += kittenTwo;
+        const cardRace = document.querySelector('.card_race');
+        cardRace.innerHTML = 'Uy que despiste, no sabemos su raza';
+      }
     }
-    if (catDescription3.includes(descrSearchText)) {
+    if (kittenData.kittenData3.desc.includes(descrSearchText)) {
+      if(kittenData.kittenData3.race != ""){
       elementList.innerHTML += kittenThree;
-    }
+    } else{
+        elementList.innerHTML += kittenThree;
+        const cardRace = document.querySelector('.card_race');
+        cardRace.innerHTML = 'Uy que despiste, no sabemos su raza';
+      }
   }
+}
 };
 
 const buttonSearch = document.querySelector(".js-button-search");
