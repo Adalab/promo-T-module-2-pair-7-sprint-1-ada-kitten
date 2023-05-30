5040e3e21dee2daada49dd3008f7be822953596d
MariaRipley <roblesb.maria@gmail.com>
2023-05-30 11:31:23 +0200

    rename folder

--- js/main.js
+++ js/main.js
@@ -28,75 +28,101 @@ const kittenData_3 = {
 };
 const kittenData = [kittenData_1, kittenData_2, kittenData_3];
 
-//crear cards con gatitos
-const kittenOne = renderKitten(kittenData[0]);
-const kittenTwo = renderKitten(kittenData[1]);
-const kittenThree = renderKitten(kittenData[2]);
-
-elementList.innerHTML = kittenOne + kittenTwo + kittenThree;
-
-function renderKitten(cat) {
-   return `<li class="card">
+function renderKittenList(kittenData) {
+  let returnKitten = ''; 
+  for (let i = 0; i < kittenData.length; i++) {
+     returnKitten += `<li class="card">
             <article>
               <img
                 class="card_img"
-                src= ${cat.image}
+                src= ${kittenData[i].image}
                 alt="siames-cat"
               />
-              <h3 class="card_title">${cat.name.toUpperCase()}</h3>
-              <h4 class="card_race">${cat.race}</h4>
+              <h3 class="card_title">${kittenData[i].name.toUpperCase()}</h3>
+              <h4 class="card_race">${kittenData[i].race}</h4>
               <p class="card_description">
-                ${cat.desc}
+                ${kittenData[i].desc}
               </p>
             </article>
           </li>`;
+  }
+  return returnKitten;
 }
+
+elementList.innerHTML = renderKittenList(kittenData);
 
 //Buscar/filtrar
 const input_search_desc = document.querySelector('.js_in_search_desc');
 const input_search_raza = document.querySelector('.js_in_search_raza');
 const cardRace = document.querySelector('.js__race');
 
-const filterKitten = (event) => {
+function filterKitten(event) {
+  debugger;
   event.preventDefault();
   elementList.innerHTML = '';
-  const descrSearchText = input_search_desc.value;
-  const raceSearchText = input_search_raza.value;
+  for (const kittenItem of kittenData) {
+    const descrSearchText = input_search_desc.value;
+    const raceSearchText = input_search_raza.value;
   if (descrSearchText != '' || raceSearchText != '') {
-    if (kittenData[0].desc.includes(descrSearchText)) {
-      if (kittenData[0].race != '') {
-        elementList.innerHTML += renderKitten(
-          kittenData[0].image,
-          kittenData[0].name,
-          kittenData[0].race,
-          kittenData[0].desc
+    if (kittenItem.desc.includes(descrSearchText)) {
+      if (kittenItem.race != '') {
+        elementList.innerHTML += renderKittenList(
+          kittenItem.image,
+          kittenItem.name,
+          kittenItem.race,
+          kittenItem.desc,
         );
       } else {
-        elementList.innerHTML += kittenOne;
-        const cardRace = document.querySelector('.card_race');
-        cardRace.innerHTML = 'Uy que despiste, no sabemos su raza';
-      }
-    }
-    if (kittenData[1].desc.includes(descrSearchText)) {
-      if (kittenData[1].race != '') {
-        elementList.innerHTML += kittenTwo;
-      } else {
-        elementList.innerHTML += kittenTwo;
-        const cardRace = document.querySelector('.card_race');
-        cardRace.innerHTML = 'Uy que despiste, no sabemos su raza';
-      }
-    }
-    if (kittenData[2].desc.includes(descrSearchText)) {
-      if (kittenData[2].race != '') {
-        elementList.innerHTML += kittenThree;
-      } else {
-        elementList.innerHTML += kittenThree;
+        elementList.innerHTML +=  renderKittenList(
+          kittenItem)
         const cardRace = document.querySelector('.card_race');
         cardRace.innerHTML = 'Uy que despiste, no sabemos su raza';
       }
     }
   }
-};
+}
+}
+
+// const filterKitten = (event) => {
+//   event.preventDefault();
+//   elementList.innerHTML = '';
+//   const descrSearchText = input_search_desc.value;
+//   const raceSearchText = input_search_raza.value;
+//   if (descrSearchText != '' || raceSearchText != '') {
+//     if (kittenData[0].desc.includes(descrSearchText)) {
+//       if (kittenData[0].race != '') {
+//         elementList.innerHTML += renderKittenList(
+//           kittenData[0].image,
+//           kittenData[0].name,
+//           kittenData[0].race,
+//           kittenData[0].desc
+//         );
+//       } else {
+//         elementList.innerHTML += kittenOne;
+//         const cardRace = document.querySelector('.card_race');
+//         cardRace.innerHTML = 'Uy que despiste, no sabemos su raza';
+//       }
+//     }
+//     if (kittenData[1].desc.includes(descrSearchText)) {
+//       if (kittenData[1].race != '') {
+//         elementList.innerHTML += kittenTwo;
+//       } else {
+//         elementList.innerHTML += kittenTwo;
+//         const cardRace = document.querySelector('.card_race');
+//         cardRace.innerHTML = 'Uy que despiste, no sabemos su raza';
+//       }
+//     }
+//     if (kittenData[2].desc.includes(descrSearchText)) {
+//       if (kittenData[2].race != '') {
+//         elementList.innerHTML += kittenThree;
+//       } else {
+//         elementList.innerHTML += kittenThree;
+//         const cardRace = document.querySelector('.card_race');
+//         cardRace.innerHTML = 'Uy que despiste, no sabemos su raza';
+//       }
+//     }
+//   }
+// };
 
 const buttonSearch = document.querySelector('.js-button-search');
 buttonSearch.addEventListener('click', filterKitten);
